3e1fc1eb5e7750d26c8e619a9a08d8bb0ad7d1e2
BogdanBratsky <bogdan_bogdanov.1@mail.ru>
2025-05-21 03:28:07 +0800

    added staff block

--- src/components/ItegoStaff.vue
+++ src/components/ItegoStaff.vue
@@ -1,9 +1,151 @@
 <template>
-    ItegoStaff
+    <section class="itego-staff">
+        <div class="container">
+            <div class="itego-staff__title">
+                Собственный штат высококвалифицированных IT-специалистов
+            </div>
+            <div class="itego-staff__subtitle">
+                Сложные задачи решают уверенно и эффективно
+            </div>
+             <ItegoStaffPerson
+                :name="staffList[currentIndex].name"
+                :post="staffList[currentIndex].post"
+                :superpower="staffList[currentIndex].superpower"
+                :facts="staffList[currentIndex].facts"
+                :photo="staffList[currentIndex].photo"
+            />
+
+            <div class="itego-staff__next-btn" @click="nextPerson">
+                <div class="itego-staff__next-btn-img">
+                    <img src="../assets/images/interface0/arrow2.svg" alt="">
+                </div>
+                следующий специалист
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
+import ItegoStaffPerson from './ItegoStaffPerson.vue';
+
 export default {
-    name: 'ItegoStaff'
+  name: 'ItegoStaff',
+  components: {
+    ItegoStaffPerson
+  },
+  data() {
+    return {
+      currentIndex: 0,
+      staffList: [
+        {
+          name: 'Владислав',
+          post: 'Специалист технической поддержки',
+          superpower: 'Всё записывает',
+          facts: [
+            'КМС по настольному теннису',
+            'Знает несколько иностранных языков',
+            'Увлекается программированием',
+            'Любит путешествовать',
+          ],
+          photo: require('../assets/images/staff/2.jpg')
+        },
+        {
+          name: 'Алексей',
+          post: 'Программист 1с',
+          superpower: 'Напишет код любой сложности и он будет рабочим',
+          facts: [
+            '15 лет улучшает код в 1с',
+            'Работает по профессии',
+            'Сначала думает, потом делает :)',
+          ],
+          photo: require('../assets/images/staff/3.jpg')
+        },
+        {
+          name: 'Богдан',
+          post: 'Специалист технической поддержки',
+          superpower: 'Самый молодой джедай',
+          facts: [
+            'С 7 лет за компами',
+            'В узких кругах известен как личный ремонтник',
+            'С 5 лет изучает Английский язык',
+            'Увлекается компьютерами, сетями и программированием',
+          ],
+          photo: require('../assets/images/staff/4.jpg')
+        },
+        {
+          name: 'Александр',
+          post: 'Специалист технической поддержки',
+          superpower: 'Реаниматор ПК',
+          facts: [
+            'В 10 лет разобрал компьютер и не смог собрать, за что получил ремня',
+            'Теперь может собрать всё что разобрал :)',
+            '5 лет помогает пользователям с «оно само так…»',
+            'Умеет хорошо готовить',
+            'Говорит что, у него IQ 140'
+          ],
+          photo: require('../assets/images/staff/1.jpg')
+        },
+        {
+          name: 'Антон',
+          post: 'Технический директор',
+          superpower: 'Где бы ни появился - там всё начинает работать…',
+          facts: [
+            '19 лет трудится в IT',
+            'Большой опыт в решении сложных задач',
+            'С детства хотел работать в IT-сфере',
+            'Увлекается плаванием и боксом'
+          ],
+          photo: require('../assets/images/staff/5.jpg')
+        },
+        {
+          name: 'Виталий',
+          post: 'Специалист технической поддержки',
+          superpower: '',
+          facts: [
+            '10 лет решает проблемы с ПК',
+            'Знает сети, как свои пальцы',
+            'СКУД? Камеры? СКС? Легко!',
+            'Завтра весь день не за ПК',
+          ],
+          photo: require('../assets/images/staff/6.jpg')
+        },
+      ]
+    };
+  },
+  methods: {
+    nextPerson() {
+      this.currentIndex = (this.currentIndex + 1) % this.staffList.length;
+    }
+  }
 }
 </script>
+
+<style lang="scss">
+.itego-staff {
+    position: relative;
+    padding: 70px 0;
+    background-color: #ffffff;
+    display: flex;
+    flex-direction: column;
+    &__title {
+        font-family: "Montserrat", sans-serif;
+        font-weight: 700;
+        font-size: 45px;
+        color: #1565C0;
+        margin-bottom: 15px;
+    }
+    &__subtitle {
+        font-family: "Montserrat", sans-serif;
+        font-weight: 400;
+        font-size: 18px;
+        margin-bottom: 41px;
+    }
+    &__next-btn {
+        margin-top: 50px;
+        align-self: flex-end;
+        user-select: none;
+        cursor: pointer;
+        float: right;
+    }
+}
+</style>
---
+++ src/components/ItegoStaffPerson.vue
@@ -0,0 +1,71 @@
+<template>
+  <section class="itego-staff-person">
+    <div class="itego-staff-person__info">
+      <div class="itego-staff-person__name">{{ name }}</div>
+      <div class="itego-staff-person__post">{{ post }}</div>
+      <div class="itego-staff-person__superpower">Суперсила: {{ superpower }}</div>
+      <ul class="itego-staff-person__facts">
+        <li v-for="(fact, index) in facts" :key="index">• {{ fact }}</li>
+      </ul>
+    </div>
+    <div class="itego-staff-person__photo-section">
+      <img :src="photo" :alt="name" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'ItegoStaffPerson',
+    props: {
+        name: String,
+        post: String,
+        superpower: String,
+        facts: Array,
+        photo: String
+    }
+}
+</script>
+
+<style lang="scss">
+.itego-staff-person {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-family: "Montserrat", sans-serif;
+    &__name {
+        font-weight: 700;
+        font-size: 24px;
+        margin-bottom: 9px;
+    }
+    &__post {
+        font-weight: 500;
+        font-size: 18px;
+        margin-bottom: 22px;
+    }
+    &__superpower {
+        font-weight: 300;
+        font-size: 18px;
+        font-style: italic;
+        margin-bottom: 27px;
+    }
+    &__facts {
+        line-height: 40px;
+        li {
+            padding-left: 10px;
+            font-weight: 400;
+            font-size: 18px;
+        }
+    }
+    &__photo-section {
+        width: 462px;
+        height: 477px;
+        box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+    }
+}
+</style>
